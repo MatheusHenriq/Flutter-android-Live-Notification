a017d09585b8afbd270816bb7caf7f705886e4d2
Matheus Henrique <matheus@obi.tec.br>
2024-09-15 20:55:42 -0300

    adding: when click notification, redirect to app

--- android/app/src/main/kotlin/com/example/android_live_notification/LiveNotificationManager.kt
+++ android/app/src/main/kotlin/com/example/android_live_notification/LiveNotificationManager.kt
@@ -5,16 +5,21 @@ import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.view.View
 import android.widget.RemoteViews
 import androidx.annotation.RequiresApi
 
-class LiveNotificationManager(private val context: Context) {
+class LiveNotificationManager(private val context: Context)  {
     private val remoteViews = RemoteViews("com.example.android_live_notification", R.layout.live_notification)
     private val  channelId = "notificationChannel"
     private val  notificationId = 100
+
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun createNotificationChannel() : NotificationManager{
@@ -32,6 +37,7 @@ class LiveNotificationManager(private val context: Context) {
     @SuppressLint("RemoteViewLayout")
     @RequiresApi(Build.VERSION_CODES.O)
     fun showNotification(currentProgress: Int, minutesToDelivery: Int) {
+
         val notificationManager =  createNotificationChannel()
 
         val notification = Notification.Builder(context, channelId)
@@ -53,10 +59,22 @@ class LiveNotificationManager(private val context: Context) {
     @RequiresApi(Build.VERSION_CODES.O)
     fun updateNotification(currentProgress: Int,minutesToDelivery: Int) {
         val notificationManager =  createNotificationChannel()
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+        }
 
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
         val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Notification.Builder(context, channelId)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(false)
+                .setOngoing(true)
                 .setContentTitle("Live Notification")
                 .setContentText("Your delivery comes in $minutesToDelivery minutes")
                 .setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
@@ -79,6 +97,7 @@ class LiveNotificationManager(private val context: Context) {
 
         val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Notification.Builder(context, channelId)
+                .setActions()
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Live Notification")
                 .setContentText("Your delivery arrive")
